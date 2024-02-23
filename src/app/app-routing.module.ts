b3d9419fda1aeb78083b69e91309b66b315c7987
David García <btkoviedo@gmail.com>
2024-02-23 14:59:35 +0100

    Fin de la sección 16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
+//Importamos las dos funciones que creamos anteriormente.
+import { canActivateGuard, canMatchGuard } from './auth/guards/auth.guard';
+import { canActivatePublicGuard, canMatchPublicGuard } from './auth/guards/public.guard';
+
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule),
+    canActivate: [canActivatePublicGuard], //Anclamos la función del canActive del public guard
+    canMatch: [canMatchPublicGuard], //Anclamos la función del canMatch del public guard
   },
   {
     path: 'heroes',
-    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule)
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule),
+    canActivate: [canActivateGuard], //Anclamos la función del canActive del auth guard
+    canMatch: [canMatchGuard], //Anclamos la función del canMatch del auth guard
   },
   {
     path: '404',
